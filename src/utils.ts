d3f4da526509e19184146075ed3e29d68608a848
VergeLyrics <123815910+vergelyrics@users.noreply.github.com>
2026-05-21 15:57:54 +0530

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,19 @@
-export function textOf(runs: any[]) {
-  return runs?.map((x: any) => x.text).join("") || ""
+export function textOf(
+  runs: any[]
+) {
+
+  return runs
+    ?.map(
+      (x: any) => x.text
+    )
+    .join("") || ""
 }
 
-export function thumbnailOf(thumbnails: any[]) {
-  return thumbnails?.at(-1)?.url || null
+export function thumbnailOf(
+  thumbnails: any[]
+) {
+
+  return thumbnails?.[
+    thumbnails.length - 1
+  ]?.url || null
 }
