8d39e07b62a732f7258802b629201afc0dc13532
VergeLyrics <123815910+vergelyrics@users.noreply.github.com>
2026-05-21 15:17:08 +0530

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Hono } from "hono"
+
+import home from "./routes/home"
+import search from "./routes/search"
+import artist from "./routes/artist"
+import album from "./routes/album"
+import playlist from "./routes/playlist"
+import related from "./routes/related"
+import player from "./routes/player"
+import stream from "./routes/stream"
+import transcript from "./routes/transcript"
+import suggestions from "./routes/suggestions"
+
+const app = new Hono()
+
+app.get("/", c => {
+  return c.json({
+    engine: "VergeTune",
+    status: "running"
+  })
+})
+
+app.route("/home", home)
+app.route("/search", search)
+app.route("/artist", artist)
+app.route("/album", album)
+app.route("/playlist", playlist)
+app.route("/related", related)
+app.route("/player", player)
+app.route("/stream", stream)
+app.route("/transcript", transcript)
+app.route("/suggestions", suggestions)
+
+export default app
