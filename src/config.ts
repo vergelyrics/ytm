eceef55568fc8e452adffc9656c7b98b28216407
VergeLyrics <123815910+vergelyrics@users.noreply.github.com>
2026-05-21 15:39:05 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,52 @@
 export const CLIENTS = {
+
   WEB_REMIX: {
+
     clientName: "WEB_REMIX",
-    clientVersion: "1.20240520.01.00",
-    apiKey: "AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX3"
+
+    /*
+      IMPORTANT:
+      numeric client ID
+    */
+
+    clientId: "67",
+
+    clientVersion:
+      "1.20240520.01.00",
+
+    apiKey:
+      "AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX3"
   },
 
   IOS: {
+
     clientName: "IOS",
-    clientVersion: "19.09.3",
-    apiKey: "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+
+    /*
+      IMPORTANT:
+      numeric client ID
+    */
+
+    clientId: "5",
+
+    clientVersion:
+      "19.09.3",
+
+    apiKey:
+      "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
   },
 
   ANDROID_MUSIC: {
-    clientName: "ANDROID_MUSIC",
-    clientVersion: "6.42.52",
-    apiKey: "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+
+    clientName:
+      "ANDROID_MUSIC",
+
+    clientId: "21",
+
+    clientVersion:
+      "6.42.52",
+
+    apiKey:
+      "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
   }
 }
