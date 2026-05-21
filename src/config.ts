7d6a451229ee5b16519cadadb442dab8a251ffb0
VergeLyrics <123815910+vergelyrics@users.noreply.github.com>
2026-05-21 15:09:19 +0530

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export const CLIENTS = {
+  WEB_REMIX: {
+    clientName: "WEB_REMIX",
+    clientVersion: "1.20240520.01.00",
+    apiKey: "AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX3"
+  },
+
+  IOS: {
+    clientName: "IOS",
+    clientVersion: "19.09.3",
+    apiKey: "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+  },
+
+  ANDROID_MUSIC: {
+    clientName: "ANDROID_MUSIC",
+    clientVersion: "6.42.52",
+    apiKey: "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+  }
+}
